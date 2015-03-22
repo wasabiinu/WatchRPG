6f485b0247eb5b10fe305eb005f6f733b2ee6ba1
横山 優 <wasabiinu@wasabiinu-no-MacBook-Pro.local>
2015-03-22 23:52:43 +0900

    Initial Commit

---
+++ WatchRPG/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WatchRPG
+//
+//  Created by 横山 優 on 2015/03/22.
+//  Copyright (c) 2015年 Yu Yokoyama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
