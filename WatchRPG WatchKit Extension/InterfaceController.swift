3695fc5898cbab46ecbf1ddf0af131ee19f292a0
横山 優 <wasabiinu@wasabiinu-no-MacBook-Pro.local>
2015-03-22 23:57:07 +0900

    プロジェクト作成

---
+++ WatchRPG WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  WatchRPG WatchKit Extension
+//
+//  Created by 横山 優 on 2015/03/22.
+//  Copyright (c) 2015年 Yu Yokoyama. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
